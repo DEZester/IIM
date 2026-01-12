7d503039e59af1d63fbaa48b3ecb829cda87f13c
DEZester <aromudez@gmail.com>
2026-01-12 10:28:46 +0200

    Add ESLint and Prettier configurations for code linting and formatting; update `package.json` with scripts and dev dependencies.

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,17 +1,15 @@
-import type {Metadata} from "next";
-import React from "react";
+import type { Metadata } from 'next';
+import React from 'react';
 
 export const metadata: Metadata = {
-    title: "IIM Web",
-    description: "Web application for IIM",
+  title: 'IIM Web',
+  description: 'Web application for IIM',
 };
 
-export default function RootLayout({
-                                       children,
-                                   }: { children: React.ReactNode }) {
-    return (
-        <html lang="en">
-        <body>{children}</body>
-        </html>
-    )
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,8 @@
-export default function Home(){
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-centern p-24">
-            <h1 className="text=4xl font-bold">IIM: Interactive Industrial Modeler</h1>
-            <p className="mt-4 text-xl">Наш 3D редактор скоро будет здесь 🚀</p>
-        </main>
-    )
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-centern p-24">
+      <h1 className="text=4xl font-bold">IIM: Interactive Industrial Modeler</h1>
+      <p className="mt-4 text-xl">Наш 3D редактор скоро будет здесь 🚀</p>
+    </main>
+  );
 }
